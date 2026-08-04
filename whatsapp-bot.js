6d1d33ded7e778ee10b497c773b4a3cdceca1aa5
ihebaoufi10-tech <ihebaoufi10@gmail.com>
2026-08-04 23:33:12 +0100

    Update whatsapp-bot.js

--- whatsapp-bot.js
+++ whatsapp-bot.js
@@ -8,14 +8,11 @@ const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
     headless: true,
+    executablePath: '/usr/bin/google-chrome',
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
       "--disable-dev-shm-usage",
-      "--disable-accelerated-2d-canvas",
-      "--no-first-run",
-      "--no-zygote",
-      "--single-process",
       "--disable-gpu"
     ],
   },
